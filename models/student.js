feef9d77a22789b7e82ea56671726e7f79651b67
chenxizi <xchen182@hawk.iit.edu>
2019-11-06 14:13:31 -0600

    add studdents parts

---
+++ models/student.js
@@ -0,0 +1,45 @@
+/**
+*  Developer model
+*  Describes the characteristics of each attribute in a developer resource.
+*
+* @author Xizi Chen
+* @requires mongoose
+*
+*/
+const mongoose = require('mongoose')
+
+const DeveloperSchema = new mongoose.Schema({
+
+  _id: {
+    type: Number,
+    required: true
+  },
+  email: {
+    type: String,
+    minlength: 5,
+    maxlength: 100,
+    required: true,
+    unique: true
+  },
+  firstname: {
+    type: String,
+    minlength: 3,
+    maxlength: 100,
+    required: false,
+    default: 'First name'
+  },
+  lastname: {
+    type: String,
+    minlength: 3,
+    maxlength: 100,
+    required: false,
+    default: 'Last name'
+  },
+  phonenumber: {
+    type: String,
+    required: false,
+    unique: ture
+  }
+
+})
+module.exports = mongoose.model('Developer', DeveloperSchema)
