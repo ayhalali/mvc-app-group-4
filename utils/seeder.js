bf37e84b6b49940880c4e8715b8119c6aa3c4306
Terry Woosley <s526788@nwmissouri.edu>
2019-11-06 18:07:50 -0600

    update seeder with sections sample data injections

--- utils/seeder.js
+++ utils/seeder.js
@@ -3,6 +3,9 @@ const Datastore = require('nedb') // set up a temporary (in memory) database
 // read in developers data file
 const developerData = require('../data/developers.json') 
 
+// read in section data file
+const sectionsData = require('../data/sections.json');
+
 // inject Express app to configure it - EVERYTHING in through argument list
 
 module.exports = (app) => {
@@ -59,9 +62,6 @@ module.exports = (app) => {
   console.log('END Data Seeder. Student data read and verified.')
 
 
-
-
-
   const courseData = require('../data/course.json')
 
   console.log('START data seeder.')
@@ -77,4 +77,20 @@ module.exports = (app) => {
   console.log(`${app.locals.students.course.length} students seeded`)
 
   console.log('END Data Seeder. Student data read and verified.')
+
+  //add section sample data to database --------------------------------------------
+
+  console.log('START data seeder for sections.')
+
+  db.sections = new Datastore() // create new table named sections
+  db.sections.loadDatabase() // call the loadDatabase method
+
+  // insert the sample data for sections into our new table
+  db.sections.insert(sectionsData)
+
+  // initialize app.locals
+  app.locals.sections = db.sections.find(sectionsData)
+  console.log(`${app.locals.sections.query.length} sections seeded`)
+
+  console.log('END Data Seeder. Sample data for sections read and verified.')
 }
